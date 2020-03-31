d11acfb5807d8ca0bfa67ab1fd28dff051fc7403
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-03-31 14:49:48 -0400

    added object and function

--- thisExercise.js
+++ thisExercise.js
@@ -1,9 +1,13 @@
 let slideshow = {
     photoList: ['mountainPic', 'oceanPic', 'desertPic', 'forrestPic'],
     currentPhotoIndex: 0,
-    function nextPhoto() { 
-        
-        
+    nextPhoto: function() { 
+            if (this.currentPhotoIndex < this.photoList.length) {
+                this.currentPhotoIndex ++;
+                console.log(this.currentPhotoIndex);
+                console.log(this.photoList);
+            }
     }
 }
+slideshow.nextPhoto()
 console.log(slideshow);
