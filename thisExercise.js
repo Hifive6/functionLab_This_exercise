84d1e47223e9c5fd95b21fc79092fb44e9d25316
David Juarez <davidjuarez@Davids-Air.attlocal.net>
2020-03-31 15:56:58 -0400

    getCurrentPhoto

--- thisExercise.js
+++ thisExercise.js
@@ -2,14 +2,28 @@ let slideshow = {
     photoList: ['mountainPic', 'oceanPic', 'desertPic', 'forrestPic'],
     currentPhotoIndex: 0,
     nextPhoto: function() { 
-            if (this.currentPhotoIndex < this.photoList.length) {
+            if (this.currentPhotoIndex < this.photoList.length - 1) {
                 this.currentPhotoIndex ++;
                 console.log(this.currentPhotoIndex);
                 console.log(this.photoList[this.currentPhotoIndex]);
             } else {
                 console.log("End of Slideshow")
             }
+    },
+    prevPhoto: function() {
+        if (this.currentPhotoIndex <= this.photoList.length){
+            this.currentPhotoIndex --;
+            console.log(this.currentPhotoIndex);
+            console.log(this.photoList[this.currentPhotoIndex]);
+        }
+    },
+    getCurrentPhoto: function() {
+        if (this.currentPhotoIndex < this.photoList.length -1) {
+            console.log(this.photoList[this.currentPhotoIndex])
+        }
     }
 }
-slideshow.nextPhoto()
+slideshow.nextPhoto();
+slideshow.prevPhoto();
+slideshow.getCurrentPhoto();
 // console.log(slideshow);
